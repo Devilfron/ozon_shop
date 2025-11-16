20e7840a9019fdf891cf892352e1653025b07b73
Devilfron <inhagni@gmail.com>
2025-11-16 14:07:48 +0100

    first commit

---
+++ js/cart.js
@@ -0,0 +1,53 @@
+const cart = () => {
+    const cartBtn = document.getElementById('cart')
+    const cartModal = document.querySelector('.cart')
+    const closeCartBtn = cartModal.querySelector('.cart-close')
+
+    const openCart = () => {
+        cartModal.style.display = 'flex'
+    }
+
+    const closeCart = () => {
+        cartModal.style.display = ''
+    }
+
+    cartBtn.addEventListener('click', openCart)
+    closeCartBtn.addEventListener('click', closeCart)
+}
+
+cart ()
+// cartBtn.style.width = '10px'
+// cartBtn.onclick = function() {
+    // console.log('clicked');
+    // openCart()
+// }
+
+// cartBtn.onclick = openCart
+
+
+
+// closeCartBtn.onclick = function() {
+//     console.log('first');
+// }
+
+// closeCartBtn.onclick = function() {
+//     console.log('second');
+// }
+
+// closeCartBtn.onclick = function() {
+//     console.log('third');
+// }
+
+// closeCartBtn.addEventListener('click', () => {
+//     console.log('first');
+// }
+
+// closeCartBtn.addEventListener('click', () => {
+//     console.log('second');
+// }
+// closeCartBtn.addEventListener('click', () => {
+//     console.log('third');
+// }
+
+// closeCartBtn.onclick = closeCart
+// console.dir(closeCartBtn);
